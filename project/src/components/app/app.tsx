5638eada5efea6755efcec0553b7699b81c6ab7c
Lisa <lisabazdyreva@gmail.com>
2021-10-01 21:55:59 +0300

    Удален файл с тестом, добавлены компоненты: карточка, главный экран. Проп количество предложений аренды корректно передается.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type AppScreenProps = {
+  rentOffersValue: number,
+}
+
+function App({rentOffersValue}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      rentOffersValue={rentOffersValue}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  RENT_OFFERS_VALUE: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      rentOffersValue = {Setting.RENT_OFFERS_VALUE}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
