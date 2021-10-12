86defe3584c7aa1e56be4cbd4cc339eeaeb65d7f
Lisa <lisabazdyreva@gmail.com>
2021-10-12 18:36:49 +0300

    Добавлен стейт для рейтинга в комментарии

--- project/src/components/comment-form/comment-form.tsx
+++ project/src/components/comment-form/comment-form.tsx
@@ -1,39 +1,81 @@
-
+import {ChangeEvent, useState} from 'react';
 
 function CommentForm(): JSX.Element {
+  const [rating, setRating] = useState(0);
+
+  const handleRatingChange = (evt:ChangeEvent<HTMLInputElement>): void => {
+    setRating(Number(evt.target.value));
+  };
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
+
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="5"
+          id="5-stars"
+          type="radio"
+          onChange={handleRatingChange}
+        />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="4"
+          id="4-stars"
+          type="radio"
+          onChange={handleRatingChange}
+        />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="3"
+          id="3-stars"
+          type="radio"
+          onChange={handleRatingChange}
+        />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="2"
+          id="2-stars"
+          type="radio"
+          onChange={handleRatingChange}
+        />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
+        <input
+          className="form__rating-input visually-hidden"
+          name="rating"
+          value="1"
+          id="1-star"
+          type="radio"
+          onChange={handleRatingChange}
+        />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
@@ -53,6 +95,7 @@ function CommentForm(): JSX.Element {
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
       </div>
+      {rating}
     </form>
   );
 }
