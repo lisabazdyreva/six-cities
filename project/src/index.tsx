203f03f59a362d330e0778a186ea58e8ceb57557
Lisa <lisabazdyreva@gmail.com>
2021-10-22 20:51:41 +0300

    Создано хранилище, передается пропсами провайдеру, подключены дев тулс

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {createStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app';
 
 import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
 
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import {reducer} from './store/reducers/reducer';
+
 const Setting = {
   RENT_OFFERS_VALUE: 312,
 };
 
+const store = createStore(reducer, composeWithDevTools());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      rentOffersValue = {Setting.RENT_OFFERS_VALUE}
-      offers={offers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        rentOffersValue = {Setting.RENT_OFFERS_VALUE}
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
