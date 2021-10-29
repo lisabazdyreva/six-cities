27114f707794ca533582d72cc6ccea224978aed7
Lisa <lisabazdyreva@gmail.com>
2021-10-30 00:01:14 +0300

    Компонент хедер подключен к хранилищу и отрисовывается в зависимости от статуса авторизации

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,11 +1,24 @@
 import {Link} from 'react-router-dom';
+import {connect, ConnectedProps} from 'react-redux';
+
+import {State} from '../../types/state';
 
 import Logo from '../logo/logo';
 
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 
 
-function Header(): JSX.Element {
+function mapStateToProps ({authorizationStatus}: State) {
+  return ({
+    authorizationStatus,
+  });
+}
+
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+
+function Header({authorizationStatus}: PropsFromRedux): JSX.Element {
   return (
     <header className="header">
       <div className="container">
@@ -13,18 +26,30 @@ function Header(): JSX.Element {
           <Logo />
           <nav className="header__nav">
             <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                </Link>
-              </li>
-              <li className="header__nav-item">
-                <Link className="header__nav-link" to={AppRoute.Login}> {/* пока так */}
-                  <span className="header__signout">Sign out</span>
-                </Link>
-              </li>
+              {
+                authorizationStatus === AuthorizationStatus.Auth ? (
+                  <>
+                    <li className="header__nav-item user">
+                      <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                        <div className="header__avatar-wrapper user__avatar-wrapper">
+                        </div>
+                        <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      </Link>
+                    </li>
+                    <li className="header__nav-item">
+                      <Link className="header__nav-link" to={AppRoute.Login}>
+                        <span className="header__signout">Sign out</span>
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <li className="header__nav-item">
+                    <Link className="header__nav-link" to={AppRoute.Login}>
+                      <span className="header__signout">Sign in</span>
+                    </Link>
+                  </li>
+                )
+              }
             </ul>
           </nav>
         </div>
@@ -33,4 +58,5 @@ function Header(): JSX.Element {
   );
 }
 
-export default Header;
+export {Header};
+export default connector(Header);
