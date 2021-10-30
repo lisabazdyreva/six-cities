0fe398862941c63434a45b79f59b1137f0b51ac6
Lisa <lisabazdyreva@gmail.com>
2021-10-30 15:20:07 +0300

    Добавлен перехватчик запроса, в нем добавляется заголовок с токеном.

--- project/src/api.ts
+++ project/src/api.ts
@@ -1,4 +1,11 @@
-import axios, {AxiosError, AxiosInstance, AxiosResponse} from 'axios';
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  AxiosRequestConfig
+} from 'axios';
+
+import {getToken} from './services/token';
 
 const BASE_URL = 'https://8.react.pages.academy/six-cities';
 const TIMEOUT = 5000;
@@ -22,6 +29,18 @@ function createAPI(onUnauthorized: UnauthorizedCb): AxiosInstance {
         return onUnauthorized();
       }
       return Promise.reject(error);
+    },
+  );
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
     },
   );
 
